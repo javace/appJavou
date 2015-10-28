ab5d5947dee1b558816045a464e3bdee516fc6ce
Daniel Baccin <daniel.baccin@gmail.com>
2015-10-28 10:30:56 -0300

    remove methods

--- app/src/main/java/br/com/javace/javou/model/participant/Participant.java
+++ app/src/main/java/br/com/javace/javou/model/participant/Participant.java
@@ -84,14 +84,6 @@ public class Participant implements Parcelable {
         this.id = id;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
     public boolean isSex() {
         return sex;
     }
